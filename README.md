ea196b0dacde9a6e4c93b0ce77ba48fda3490d8d
bilalmotiwala <63motiwalas@gmail.com>
2023-08-07 14:40:34 +0800

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pyme-token-contracts
-A repository to maintain the Pyme Token Contract and the version history for the said token contract.
+# Pyme Token Contracts
+This repository is an archive to manage the versioning of the Pyme Token Contract as it evolves and continues to be upgraded and updated overtime to suit feasibility and functionality of the PYME token.
+
+### Token Information
+| Item | Description |
+| --- | --- |
+| Token Name | PymeDAO |
+| Token Symbol | PYME |
+| Decimals | 18 |
+| Contract Address | 0x3408636A7825E894ac5521cA55494F89F96DF240 |
+| Supported Networks | Ethereum Mainnet |
+| Deployment Version | v0.2 |
+| Upgrade Implementation | UUPS |
+
+### Changelog
+6th August, 2023
+- Contract Upgrade from v0.1 to v0.2. Implementing Whitelists, Blacklist and Token Pausability following Community Votes
+
+30th June, 2023
+- Deployment and launch of Pyme Token Contract
---
+++ versions/v0.1/PymeToken.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+/*
+Contract: PYME Token Smart Contract
+Version: v0.1
+Date: 25th of June, 2023
+Author: Bilal Motiwala
+Security Contact: bilal@pyme.id
+
+===============================================
+Built Using OpenZeppelin's Contract Wizard v4.x
+===============================================
+*/
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./ERC20CappedUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract PymeToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, UUPSUpgradeable, ERC20CappedUpgradeable {
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("PymeDAO", "PYME");
+        __ERC20Burnable_init();
+        __ERC20Snapshot_init();
+        __Ownable_init();
+        __Pausable_init();
+        __ERC20Permit_init("PymeDAO");
+        __ERC20Votes_init();
+        __UUPSUpgradeable_init();
+        __ERC20Capped_init(1000000000 * 10 ** decimals());
+        _mint(msg.sender, 510000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable, ERC20CappedUpgradeable)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._burn(account, amount);
+    }
+}
